c5661f86fc5ba0566cecece550f5d48d0d17668e
Gurtek <gurtek@protonmail.com>
2017-12-03 13:18:24 +0530

    Inital project dagger 2 and conductor added

---
+++ app/src/main/java/com/singh/gurtek/kotlinconductor/di/AppComponent.kt
@@ -0,0 +1,29 @@
+package com.singh.gurtek.kotlinconductor.di
+
+import android.app.Application
+import com.singh.gurtek.kotlinconductor.base.KotlinApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+
+
+/**
+ * Created by Gurtek singh
+ * on 12/3/2017
+ * gurtek@protonmail.com
+ */
+@ApplicationScope
+@Component(modules = arrayOf(AndroidInjectionModule::class,
+        AppModule::class, ActivityBuilder::class))
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(kotlinApp: KotlinApp)
+}
