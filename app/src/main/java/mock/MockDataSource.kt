d9190c6153ac7e2a748e7d85c8b74a022ee4ffc0
Gurtek <gurtek@protonmail.com>
2018-01-04 18:20:13 +0530

    Condutor injector added

--- app/src/main/java/mock/MockDataSource.kt
+++ app/src/main/java/mock/MockDataSource.kt
@@ -1,16 +1,8 @@
 package mock
 
-import com.singh.gurtek.kotlinconductor.models.FlickerResponse
-
 /**
  * Created by Gurtek singh
  * on on 12/13/2017
  * gurtek@protonmail.com
  */
-object MockDataSource {
-
-    fun getMockPhotos(): FlickerResponse {
-
-        return FlickerResponse()
-    }
-}
+object MockDataSource
